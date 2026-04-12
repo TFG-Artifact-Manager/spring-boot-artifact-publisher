1f92e6fd63ffa4a80780fa4a25b8058c4cdbceb8
NicolasVO-dot <nicolasvillanuevaortega@gmail.com>
2026-04-12 15:55:54 +0300

    fdfs

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -13,6 +13,7 @@ import java.util.Map;
 public class DemoApplication {
 
     public static void main(String[] args) {
+        //dfsd
         SpringApplication.run(DemoApplication.class, args);
     }
 
